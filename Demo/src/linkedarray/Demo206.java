3f5974712ec0a583bca98c44e9cda98c1a7cd7bf
liuminchao123 <mr.liuminchao@outlook.com>
2020-07-19 22:43:06 +0800

    2020.07.19

--- Demo/src/linkedarray/Demo206.java
+++ Demo/src/linkedarray/Demo206.java
@@ -1,13 +1,30 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
 class Solution {
     public ListNode reverseList(ListNode head) {
+    	// 检验链表是否为空或只有一个元素
+        if(head == null || head.next == null)
+            return head;
+        // 创建指针指向最后一个元素  即变化后的首元素
+    	ListNode newHead = head;
+    	while(newHead.next != null)
+    		newHead = newHead.next;
+    	// 创建指针  迭代到倒数第二位元素
+    	ListNode node = head;
+    	while(node.next.next != null)
+    		node = node.next;
+    	node.next.next = node;
+    	node.next = null;   
+    	node = head;
+    	if (node.next != null)  // 说明倒排还没有完成
+    		reverseList(node);
+
+    	return newHead;   // 返回新的头节点
 
     }
 }
--- Demo/src/linkedarray/Demo206_1.java
+++ Demo/src/linkedarray/Demo206_1.java
@@ -1,1 +1,21 @@
-
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+class Solution {
+	public ListNode reverseList(ListNode head) {
+		// 检验链表是否为空或只有一个元素
+        if(head == null || head.next == null)
+            return head;
+		ListNode pre = null;
+		ListNode cur = head;
+		while (cur.next != null){
+			ListNode nextNode = cur.next;
+			cur.next = pre;
+			pre = cur;
+			cur = nextNode;
+		}
+		return cur;
+	}
+}
