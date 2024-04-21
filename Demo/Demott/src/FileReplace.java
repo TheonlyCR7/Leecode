42f0402ba6672773313e3c5c00e712a452791863
小火锅好好吃 <58215622+TheonlyCR7@users.noreply.github.com>
2024-04-21 22:05:01 +0800

    替换文本小程序

---
+++ Demo/Demott/src/FileReplace.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+
+public class FileReplace {
+    public static void main(String[] args) {
+        Path inputPath = Paths.get("D:\\Document\\school_doc\\paper_per\\inf_se\\en.txt"); // 输入文件路径
+        Path outputPath = Paths.get("D:\\Document\\school_doc\\paper_per\\inf_se\\en_output.txt"); // 输出文件路径
+
+        try {
+            String content = new String(Files.readAllBytes(inputPath), StandardCharsets.UTF_8);
+            content = content.replace("\r", " ").replace("\n", " ");
+
+            Files.write(outputPath, content.getBytes(StandardCharsets.UTF_8));
+            System.out.println("文件内容替换完成并已写入到 " + outputPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
